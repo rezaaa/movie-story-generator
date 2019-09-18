b48e533c92c44cc5dccbac1d72843bc89e068cab
Mohammad Reza <rezamahmudy1@gmail.com>
2019-09-18 21:16:07 +0430

    update package

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Home from './containers/home/Home';
 import styles from './assets/styles/general.scss';
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga'
+import {
+  applyMiddleware,
+  createStore as createReduxStore,
+  compose
+} from "redux";
+import createSagaMiddleware, { END } from "redux-saga";
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/main';
 import rootSaga from './sagas/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware()
-let store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(sagaMiddleware),
+const composeEnhancers =
+(
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+compose;
+const sagaMiddleware = createSagaMiddleware();
+const store = createReduxStore(
+rootReducer,
+composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+store.runSaga = sagaMiddleware.run(rootSaga);
+store.close = () => store.dispatch(END);
 
-sagaMiddleware.run(rootSaga)
 
 class AppToLoad extends Component {
     render() {
