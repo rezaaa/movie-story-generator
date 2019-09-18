d105ca1561844c9281aa647adbc8c58332842ade
Mohammad Reza <rezamahmudy1@gmail.com>
2019-09-18 20:29:37 +0430

    change proxy

--- common/urls.js
+++ common/urls.js
@@ -3,7 +3,7 @@ const env = 'pro';
 // poster
 const posterUrlBase = {
   dev: 'http://image.tmdb.org',
-  pro: 'https://sazito.cloud.tyk.io/posters',
+  pro: 'https://murmuring-tundra-31743.herokuapp.com/posters',
 };
 export const posterUrl1280 = `${posterUrlBase[env]}/t/p/w1280/`;
 export const posterUrl600 = `${posterUrlBase[env]}/t/p/w600_and_h900_bestv2/`;
@@ -13,7 +13,7 @@ export const posterUrl185 = `${posterUrlBase[env]}/t/p/w185_and_h278_bestv2/`;
 // api
 const apiBase = {
   dev: 'https://api.themoviedb.org',
-  pro: 'https://sazito.cloud.tyk.io/movies',
+  pro: 'https://murmuring-tundra-31743.herokuapp.com/movies',
 };
 export const moviesApiBase = apiBase[env];
 export const apiKey = '4ba2c80bd43f2892ecb3349fa445015f';
