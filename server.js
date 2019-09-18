186cfb57db643d43b737bedb80b17959e66e68a2
Mohammad Reza <rezamahmudy1@gmail.com>
2019-09-18 21:48:54 +0430

    update webpack

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var config = require("./webpack.config");
 const port = process.env.PORT || 3000;
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
+  disableHostCheck: true
 }).listen(port, "0.0.0.0", function(err, result) {
   if (err) {
     console.log(err);
