19d310f96c2e17bae4098b0ee7959279286a8f2c
Mohammad Reza <rezamahmudy1@gmail.com>
2019-12-17 17:52:57 +0330

    change api

--- common/urls.js
+++ common/urls.js
@@ -2,8 +2,8 @@ const env = 'pro';
 
 // poster
 const posterUrlBase = {
-  dev: 'https://poster.instagram.ir',
-  pro: 'https://poster.instagram.ir',
+  dev: 'https://murmuring-tundra-31743.herokuapp.com/posters',
+  pro: 'https://murmuring-tundra-31743.herokuapp.com/posters',
 };
 export const posterUrl1280 = `${posterUrlBase[env]}/t/p/w1280`;
 export const posterUrl600 = `${posterUrlBase[env]}/t/p/w600_and_h900_bestv2`;
@@ -12,8 +12,8 @@ export const posterUrl185 = `${posterUrlBase[env]}/t/p/w185_and_h278_bestv2`;
 
 // api
 const apiBase = {
-  dev: 'https://api.instagram.ir',
-  pro: 'https://api.instagram.ir',
+  dev: 'https://murmuring-tundra-31743.herokuapp.com/movies',
+  pro: 'https://murmuring-tundra-31743.herokuapp.com/movies',
 };
 export const moviesApiBase = apiBase[env];
 export const apiKey = '4ba2c80bd43f2892ecb3349fa445015f';
