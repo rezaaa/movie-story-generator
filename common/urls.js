edc52ec55979eefb355bea2ecdedb2f516074b0b
Reza <rezamahmudy1@gmail.com>
2023-05-25 22:56:43 +0330

    change proxy

--- common/urls.js
+++ common/urls.js
@@ -2,8 +2,8 @@ const env = 'pro';
 
 // poster
 const posterUrlBase = {
-  dev: 'https://murmuring-tundra-31743.herokuapp.com/posters',
-  pro: 'https://murmuring-tundra-31743.herokuapp.com/posters',
+  dev: 'https://api.themoviedb.org/',
+  pro: 'https://api.themoviedb.org/',
 };
 export const posterUrl1280 = `${posterUrlBase[env]}/t/p/w1280`;
 export const posterUrl600 = `${posterUrlBase[env]}/t/p/w600_and_h900_bestv2`;
@@ -12,8 +12,8 @@ export const posterUrl185 = `${posterUrlBase[env]}/t/p/w185_and_h278_bestv2`;
 
 // api
 const apiBase = {
-  dev: 'https://murmuring-tundra-31743.herokuapp.com/movies',
-  pro: 'https://murmuring-tundra-31743.herokuapp.com/movies',
+  dev: 'https://image.tmdb.org',
+  pro: 'https://image.tmdb.org',
 };
 export const moviesApiBase = apiBase[env];
 export const apiKey = '4ba2c80bd43f2892ecb3349fa445015f';
